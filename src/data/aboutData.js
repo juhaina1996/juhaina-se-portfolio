755aca909cc4d67fd6028f3dfaa9cb342a1d3633
juhaina1996 <juhaina.mt@rariti.io>
2023-10-28 16:24:19 +0400

    Removed projects for testing

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -3,7 +3,7 @@ export const aboutData = {
   description1: "My name is Juhaina. I'm a software developer from India",
 
   description2:
-    "I work as a software developer during the day, and in my free time, I actively contribute to freelance projects and work on personal software development projects. I also find joy in writing articles and poetry as a creative outlet. Recently, I've delved into the intriguing realms of machine learning and AI. My dedication to learning is unwavering, and I'm constantly motivated to explore new frontiers in technology.",
+    "I work as a software developer during the day, and in my free time, I actively contribute to freelance projects and work on personal software development projects. I'm dedicated to learning and constantly motivated to explore new frontiers in technology. My passion for coding and continuous improvement drives my commitment to shaping innovative solutions.",
 
   image: 2,
 };
