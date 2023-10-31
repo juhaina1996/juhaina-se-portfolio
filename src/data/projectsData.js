c3dc5fcf5fb3c4dff895f92a45f821e556a18efa
juhaina1996 <juhaina.mt@rariti.io>
2023-10-31 11:57:03 +0400

    Added invoice genartor project

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -75,16 +75,16 @@ export const projectsData = [
     demo: " https://crypto-price-id8v.onrender.com/",
     image: seven,
   },
-  //   {
-  //     id: 8,
-  //     projectName: "Car Pooling System",
-  //     projectDesc:
-  //       "The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution",
-  //     tags: ["Flutter", "React"],
-  //     code: "https://github.com/hhhrrrttt222111/developer-portfolio",
-  //     demo: "https://github.com/hhhrrrttt222111/developer-portfolio",
-  //     image: eight,
-  //   },
+  {
+    id: 8,
+    projectName: "Invoice Generator",
+    projectDesc:
+      "Automated invoice generator for seamless billing and financial management",
+    tags: ["React"],
+    code: "https://github.com/juhaina1996/invoice-generator",
+    demo: "https://invoice-generator-vj5j.onrender.com",
+    image: eight,
+  },
 ];
 
 // Do not remove any fields.
