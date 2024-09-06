dea9bb6111e6c3c330675f9c54670286d4581329
juhaina1996 <juhaina.mt@rariti.io>
2024-09-06 10:22:23 +0530

    Added one more project in the project list

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,62 +1,19 @@
-import one from "../assets/svg/projects/one.svg";
-import two from "../assets/svg/projects/two.svg";
-import three from "../assets/svg/projects/three.svg";
-import four from "../assets/svg/projects/four.svg";
 import five from "../assets/svg/projects/five.svg";
 import six from "../assets/svg/projects/six.svg";
 import seven from "../assets/svg/projects/seven.svg";
 import eight from "../assets/svg/projects/eight.svg";
 
 export const projectsData = [
-  //   {
-  //     id: 1,
-  //     projectName: "Employee Management",
-  //     projectDesc:
-  //       "This project aims to build an employee management system that consolidates all information of a company",
-  //     tags: ["React", "CSS", "Material Ui"],
-  //     code: "https://github.com/hhhrrrttt222111/developer-portfolio",
-  //     demo: "https://github.com/hhhrrrttt222111/developer-portfolio",
-  //     image: one,
-  //   },
-  //   {
-  //     id: 2,
-  //     projectName: "Railway Tracking",
-  //     projectDesc:
-  //       "This project is a railway tracking system that allows users to obtain relevant information about train timing.",
-  //     tags: ["Django", "Tailwind CSS"],
-  //     code: "https://github.com/hhhrrrttt222111/developer-portfolio",
-  //     demo: "https://github.com/hhhrrrttt222111/developer-portfolio",
-  //     image: two,
-  //   },
-  //   {
-  //     id: 3,
-  //     projectName: "Weather App",
-  //     projectDesc:
-  //       "Weather forecast systems and applications predict weather conditions based on multiple parameters.",
-  //     tags: ["Django", "CSS", "Material Ui"],
-  //     code: "https://github.com/hhhrrrttt222111/developer-portfolio",
-  //     demo: "https://github.com/hhhrrrttt222111/developer-portfolio",
-  //     image: three,
-  //   },
-  //   {
-  //     id: 4,
-  //     projectName: "Android Patient Tracker",
-  //     projectDesc:
-  //       "This project involves the development of an Android application for viewing and managing patient data.",
-  //     tags: ["Flutter", "Firebase"],
-  //     code: "https://github.com/hhhrrrttt222111/developer-portfolio",
-  //     demo: "https://github.com/hhhrrrttt222111/developer-portfolio",
-  //     image: four,
-  //   },
-  //   {
-  //     id: 5,
-  //     projectName: "E-Commerce App",
-  //     projectDesc: "A Simple E-commerce application",
-  //     tags: ["React Native", "Firebase"],
-  //     code: "https://github.com/hhhrrrttt222111/developer-portfolio",
-  //     demo: "https://github.com/hhhrrrttt222111/developer-portfolio",
-  //     image: five,
-  //   },
+  {
+    id: 6,
+    projectName: "Food Delivery App",
+    projectDesc: "Food Delivery App",
+    tags: ["React, Node js, Mongo DB"],
+    code: "https://github.com/juhaina1996/food-del",
+    demo: "https://food-del-fronend-glgs.onrender.com",
+    image: five,
+  },
+
   {
     id: 6,
     projectName: "Typing Speed",
